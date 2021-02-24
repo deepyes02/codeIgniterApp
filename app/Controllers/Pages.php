08533714c9832d7c6c1d2afa4adc738f000ece03
deepyes02 <deepyes@outlook.com>
2021-02-24 23:47:19 +0545

    made basic routing happen

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Pages extends Controller {
+    /**
+    * public function index to take command when the root page is requested.
+    * Here, for example, it's the index of pages
+    * @param none
+    * @return $string [view]
+    * @since 1.0.0
+     */
+    public function index(){
+        //returns php from Views folder with the name as string
+        return view('pages_archive');
+    }
+
+    /**
+    * custom public function that returns requested pages as available inside
+    * the views sub-directory. 
+    * 
+    * In this example, $page = home means that home.php will be served when 
+    * page/home is requested.
+    * @param string assigned to a variable
+     */
+
+    public function view($page = 'home'){
+        //if the page doesn't exist on the views
+        if (! is_file(APPPATH.'/Views/pages/'.$page.'.php')){
+            throw new \CodeIgniter\Exceptions\PageNotFoundException(($page));
+        }
+
+        $data['title'] = ucfirst($page); //capitalize first letter
+
+        echo view('templates/header', $data);
+        echo view('pages/'.$page, $data);
+        echo view('templates/footer', $data);
+    }
+}
---
+++ app/Views/pages_archive.php
@@ -0,0 +1,4 @@
+<h2>Archive for pages.</h2>
+<p>
+This is triggered when the index is called upon. We could design it so that an archive of the list of pages can be seen. Just like wordpress.
+</p>
